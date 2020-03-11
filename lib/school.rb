4a873ceebad188a03b0fd781ed370828103dbb28
Vivica Ma <vivicacongma@gmail.com>
2020-03-11 02:07:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -17,14 +17,6 @@ class School
   #   roster[grade]
   # end
   
-  
-    def add_student(student_name, grade)
-    roster[grade] ||= []
-    roster[grade] << student_name
-  end
-
-  def grade(student_grade)
-    roster[student_grade]
   end
   
 end 
