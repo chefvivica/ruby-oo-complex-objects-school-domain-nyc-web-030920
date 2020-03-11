a0c1384d696def805768ff18fb0b03b8549eb5a7
Vivica Ma <vivicacongma@gmail.com>
2020-03-11 02:34:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,23 +7,23 @@ class School
     @roster = {}
   end
   
-  # def add_student(name, grade)
-    # @name.roster.each do |k,v|
-    #     new_k = []
-    #     if !v 
-    #       new_k == k
-    #     else 
-    #       new_k << k 
-    #     end 
-    # end 
+  def add_student(name, grade)
+    @name.roster.each do |k,v|
+        new_k = []
+        if !v 
+          new_k == k
+        else 
+          new_k << k 
+        end 
+    end 
     
-  def add_student(student_name, grade)
-    roster[grade] ||= []
-    roster[grade] << student_name
-  end
+  # def add_student(student_name, grade)
+  #   roster[grade] ||= []
+  #   roster[grade] << student_name
+  # end
 
-  def grade(student_grade)
-    roster[student_grade]
+  # def grade(student_grade)
+  #   roster[student_grade]
   end
 
   
