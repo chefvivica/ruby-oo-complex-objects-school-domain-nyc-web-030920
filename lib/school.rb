13edcf87801d0d7e6c7fe542bb605664edbf8ad9
Vivica Ma <vivicacongma@gmail.com>
2020-03-11 01:48:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,9 +9,6 @@ class School
   end 
   
   def add_student(name, grade)
-    @name = name 
-    @name["new_key"] = []
-    @name["new_key"] << "new_value_for_value_array"
-    @name=> {"new_key"=>["new_value_for_value_array"]}
+    
   end 
 end 
